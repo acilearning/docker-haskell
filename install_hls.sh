b5c2aaa3fa67caa70049cf780c5c5cbd609f250c
codygman <cody@codygman.dev>
2022-09-06 15:19:05 -0500

    try -j1 to compile hls with actions

--- install_hls.sh
+++ install_hls.sh
@@ -4,7 +4,7 @@ set -o errexit -o xtrace;
 
 if [[ "$GHC_VERSION" == "9.2.4" ]]; then
     if test -n "$HLS_VERSION"; then
-	ghcup compile hls -j4 -g 26a6ca607930b85bb1a2491b8deaf1a363c2e3df --ghc "$GHC_VERSION" --cabal-update
+	ghcup compile hls -j1 -g 26a6ca607930b85bb1a2491b8deaf1a363c2e3df --ghc "$GHC_VERSION" --cabal-update
     else
 	ghcup install hls "$HLS_VERSION" --set
     fi
