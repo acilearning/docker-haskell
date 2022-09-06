b75511e1f764ad026e271c3916269e299b78da45
codygman <cody@codygman.dev>
2022-09-06 10:12:02 -0500

    [sc-126569] remove line continuations in script
    Weird this works fine for me locally

--- install_hls.sh
+++ install_hls.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-set -o errexit -o xtrace; \
-  if [[ "$GHC_VERSION" == "9.2.4" ]]; then \
-    if test -n "$HLS_VERSION"; then \
-    ghcup compile hls -j4 -g 26a6ca607930b85bb1a2491b8deaf1a363c2e3df --ghc $GHC_VERSION --cabal-update; \
-    else \
-      ghcup install hls "$HLS_VERSION" --set; \
+set -o errexit -o xtrace;
+
+if [[ "$GHC_VERSION" == "9.2.4" ]]; then
+    if test -n "$HLS_VERSION"; then
+	ghcup compile hls -j4 -g 26a6ca607930b85bb1a2491b8deaf1a363c2e3df --ghc "$GHC_VERSION" --cabal-update
+    else
+	ghcup install hls "$HLS_VERSION" --set
     fi
-  ghcup gc --hls-no-ghc; \
-  haskell-language-server-wrapper --version; \
-  fi
+    ghcup gc --hls-no-ghc;
+    haskell-language-server-wrapper --version
+fi
